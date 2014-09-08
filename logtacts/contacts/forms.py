2ef6838e3dfc830fa98e27db0829f6ad4ac5d9f4
Philip James <pjj@philipjohnjames.com>
2014-09-08 00:01:23 -0700

    Clean up the frontend a bit.

--- logtacts/contacts/forms.py
+++ logtacts/contacts/forms.py
@@ -2,20 +2,27 @@ from django import forms
 
 from contacts.models import Contact, LogEntry, Tag
 
-class ContactForm(forms.ModelForm):
+class BootstrapForm(object):
+
+    def __init__(self, *args, **kwargs):
+        super(BootstrapForm, self).__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] = 'form-control'
+
+class ContactForm(BootstrapForm, forms.ModelForm):
 
     class Meta:
         model = Contact
 
 
-class LogEntryForm(forms.ModelForm):
+class LogEntryForm(BootstrapForm, forms.ModelForm):
 
     class Meta:
         model = LogEntry
         fields = ['kind','link','time','notes']
  
 
-class TagForm(forms.ModelForm):
+class TagForm(BootstrapForm, forms.ModelForm):
 
     class Meta:
         model = Tag
