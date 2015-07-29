63d74ee66e9fe05c07d0032690889b3991e32e1b
Philip James <pjj@philipjohnjames.com>
2015-07-29 14:42:48 -0700

    Adjust settings to use specific db url

--- logtacts/settings/heroku.py
+++ logtacts/settings/heroku.py
@@ -4,7 +4,7 @@ import dj_database_url
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
-DATABASES['default'] = dj_database_url.config()
+DATABASES['default'] = dj_database_url.parse(get_env_variable('LOGTACTS_DB_URL'))
 
 SECRET_KEY = get_env_variable("SECRET_KEY")
 
