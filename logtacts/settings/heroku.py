a194bf8ab947d1b32e1a0ea8fa3a5c0dad34cddf
Philip James <pjj@philipjohnjames.com>
2015-07-29 15:10:42 -0700

    Make debug true

--- logtacts/settings/heroku.py
+++ logtacts/settings/heroku.py
@@ -1,7 +1,7 @@
 from .base import *
 import dj_database_url
 
-DEBUG = False
+DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 DATABASES['default'] = dj_database_url.parse(get_env_variable('LOGTACTS_DB_URL'))
