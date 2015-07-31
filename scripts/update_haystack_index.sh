3927de49b85d55bb9706dad0c3f111e170179f49
Philip James <pjj@philipjohnjames.com>
2015-07-31 16:24:32 -0700

    Add script for updating index

---
+++ scripts/update_haystack_index.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+source /home/deploy/.bashrc
+cd /home/deploy/Env/logtacts
+source bin/activate
+cd /home/deploy/logtacts
+python manage.py update_index --age=1 --settings=logtacts.settings.heroku
+deactivate
