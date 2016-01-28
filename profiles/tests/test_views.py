d89715196ba79da02a997688414dfa283bee5aeb
Philip James <pjj@philipjohnjames.com>
2016-01-27 16:13:13 -0800

    Add tests for user review view

--- profiles/tests/test_views.py
+++ profiles/tests/test_views.py
@@ -3,7 +3,10 @@ from django.test import TestCase
 from django.test.client import RequestFactory
 from utils.factories import UserFactory
 
-from profiles.views import ProfileView
+from profiles.views import (
+    ProfileView,
+    ReviewUserView,
+)
 
 
 class ProfileViewTests(TestCase):
@@ -19,3 +22,30 @@ class ProfileViewTests(TestCase):
 
     def test_profile_view_renders(self):
         self.response.render()
+
+
+class ReviewUserViewTests(TestCase):
+
+    def setUp(self):
+        request_factory = RequestFactory()
+        self.request = request_factory.get('/admin/dashboard/')
+
+    def test_review_user_view_200(self):
+        user = UserFactory.create()
+        user.is_staff = True
+        self.request.user = user
+        response = ReviewUserView.as_view()(self.request)
+        self.assertEqual(response.status_code, 200)
+
+    def test_review_user_view_200(self):
+        user = UserFactory.create()
+        user.is_staff = True
+        self.request.user = user
+        response = ReviewUserView.as_view()(self.request)
+        response.render()
+
+    def test_review_user_view_not_staff(self):
+        user = UserFactory.create()
+        self.request.user = user
+        response = ReviewUserView.as_view()(self.request)
+        self.assertEqual(response.status_code, 302)
