95defeb298f771e7fe8c2c6317b78f0acdf362d8
Philip James <pjj@philipjohnjames.com>
2016-01-31 15:25:49 -0800

    Update serializers, remove unnecessary tests

--- contacts/api/serializers.py
+++ contacts/api/serializers.py
@@ -5,17 +5,17 @@ from contacts import models
 class ContactSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Contact
-        fields = ('id', 'book', 'name', 'email', 'twitter', 'tumblr', 'website',
-            'cell_phone', 'home_phone', 'company', 'address', 'notes', 'tags')
+        fields = ('id', 'book', 'name', 'email', 'twitter', 'tumblr', 'website', 'portfolio',
+            'cell_phone', 'home_phone', 'company', 'address', 'notes', 'tags', 'created', 'changed')
 
 
 class TagSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Tag
-        fields = ('id', 'book', 'tag', 'color')
+        fields = ('id', 'book', 'tag', 'color', 'created', 'changed')
 
 
 class LogSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.LogEntry
-        fields = ('id', 'contact', 'kind', 'link', 'time', 'location', 'notes')
+        fields = ('id', 'contact', 'kind', 'link', 'time', 'location', 'notes', 'logged_by', 'created', 'changed')
