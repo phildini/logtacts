707cdbbd076068f1ee2b07635cc9acbdcc673029
Philip James <pjj@philipjohnjames.com>
2016-01-31 16:11:39 -0800

    Have the ContactSerialize also return logs

--- contacts/api/serializers.py
+++ contacts/api/serializers.py
@@ -2,20 +2,24 @@ from rest_framework import serializers
 from contacts import models
 
 
+class LogSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.LogEntry
+        fields = ('id', 'contact', 'kind', 'link', 'time', 'location', 'notes',
+            'logged_by', 'created', 'changed')
+
+
 class ContactSerializer(serializers.ModelSerializer):
+    logentry_set = LogSerializer(many=True, read_only=True)
+
     class Meta:
         model = models.Contact
-        fields = ('id', 'book', 'name', 'email', 'twitter', 'tumblr', 'website', 'portfolio',
-            'cell_phone', 'home_phone', 'company', 'address', 'notes', 'tags', 'created', 'changed')
+        fields = ('id', 'book', 'name', 'email', 'twitter', 'tumblr', 'website', 
+            'portfolio', 'cell_phone', 'home_phone', 'company', 'address',
+            'notes', 'tags', 'created', 'changed', 'logentry_set')
 
 
 class TagSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Tag
         fields = ('id', 'book', 'tag', 'color', 'created', 'changed')
-
-
-class LogSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = models.LogEntry
-        fields = ('id', 'contact', 'kind', 'link', 'time', 'location', 'notes', 'logged_by', 'created', 'changed')
