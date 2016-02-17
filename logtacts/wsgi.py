1e08d2f3b9f1c870683884f9db5f19049a3b1d2e
Philip James <pjj@philipjohnjames.com>
2016-02-16 21:29:39 -0800

    Wrap wsgi app in newrelic

--- logtacts/wsgi.py
+++ logtacts/wsgi.py
@@ -7,8 +7,13 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
+import newrelic.agent
 import os
+
+newrelic.agent.initialize('/home/deploy/logtacts/newrelic.ini')
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "logtacts.prod_settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+application = newrelic.agent.wsgi_application()(application)
