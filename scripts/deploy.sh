2b3419a420eee3be0b713cfdbba8c8630200dc0a
Philip James <pjj@philipjohnjames.com>
2016-02-16 22:28:19 -0800

    Keep trying

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,8 +6,4 @@ source .env
 git pull --rebase
 pip install -r requirements.txt
 kill -HUP `cat /tmp/logtacts-master.pid`
-curl https://intake.opbeat.com/api/v1/organizations/$OPBEAT_ORG_ID/apps/$OPBEAT_APP_ID/releases/ \
-    -H "Authorization: Bearer $OPBEAT_SECRET_KEY" \
-    -d rev=`git log -n 1 --pretty=format:%H` \
-    -d branch=`git rev-parse --abbrev-ref HEAD` \
-    -d status=completed
+curl https://intake.opbeat.com/api/v1/organizations/$OPBEAT_ORG_ID/apps/$OPBEAT_APP_ID/releases/ -H "Authorization: Bearer $OPBEAT_SECRET_KEY" -d rev=`git log -n 1 --pretty=format:%H` -d branch=`git rev-parse --abbrev-ref HEAD` -d status=completed
