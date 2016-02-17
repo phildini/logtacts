eff183a4136d9b7d4e676bbe739d1b43a5c7634a
Philip James <pjj@philipjohnjames.com>
2016-02-16 22:08:41 -0800

    Remove newrelic

--- logtacts/wsgi.py
+++ logtacts/wsgi.py
@@ -6,14 +6,9 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
-
-import newrelic.agent
 import os
-
-newrelic.agent.initialize('/home/deploy/logtacts/newrelic.ini')
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "logtacts.prod_settings")
 
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
 application = newrelic.agent.WSGIApplicationWrapper(application)
