eaf3c47f15833e1089c4d4068b094f9e48c8ea91
Philip James <pjj@philipjohnjames.com>
2016-02-29 15:21:50 -0800

    Improve admin display for contacts

--- contacts/admin.py
+++ contacts/admin.py
@@ -8,8 +8,24 @@ from .models import (
     LogEntry,
 )
 
-admin.site.register(Contact, SimpleHistoryAdmin)
-admin.site.register(Tag, SimpleHistoryAdmin)
+class ContactAdmin(SimpleHistoryAdmin):
+    list_display = (
+        'name',
+        'book',
+        'created',
+        'changed'
+    )
+
+class TagAdmin(SimpleHistoryAdmin):
+    list_display = (
+        'tag',
+        'book',
+        'created',
+        'changed',
+    )
+
+admin.site.register(Contact, ContactAdmin)
+admin.site.register(Tag, TagAdmin)
 admin.site.register(Book, SimpleHistoryAdmin)
 admin.site.register(BookOwner, SimpleHistoryAdmin)
 admin.site.register(LogEntry, SimpleHistoryAdmin)
