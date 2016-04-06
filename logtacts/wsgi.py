d809bbadf2cca69d660b4f1373a54daf1bc6a49c
Philip James <pjj@philipjohnjames.com>
2016-04-05 19:07:41 -0700

    Use daphne instead, and hope.

--- logtacts/wsgi.py
+++ logtacts/wsgi.py
@@ -7,9 +7,8 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 import os
-from asgiref.wsgi import WsgiToAsgiAdapter
-from channels.asgi import get_channel_layer
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "logtacts.prod_settings")
-channel_layer = get_channel_layer()
-application = WsgiToAsgiAdapter(channel_layer)
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
