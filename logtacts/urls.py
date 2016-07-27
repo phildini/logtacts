fa0d3ad3bc3fb2fcc6e3b8ee3df911df04548585
Philip James <pjj@philipjohnjames.com>
2016-07-26 19:07:24 -0700

    Restrict to exactly the right auth urls.

--- logtacts/urls.py
+++ logtacts/urls.py
@@ -2,7 +2,13 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.views.generic import TemplateView
 
+
+# Why are we doing this crazy thing of spelling out all the allauth urls?
+# Because I want to know _exactly_ what the surface area of the product is.
 from allauth.account import views as allauth_views
+from allauth.socialaccount import views as social_views
+from allauth.socialaccount.providers.twitter import views as twitter_views
+from allauth.socialaccount.providers.foursquare import views as foursquare_views
 
 import nexus
 import gargoyle
@@ -26,15 +32,69 @@ from profiles.views import ReviewUserView
 admin.autodiscover()
 gargoyle.autodiscover()
 
+
+foursquare_urls = [
+    url('^login/$', foursquare_views.oauth2_login, name="foursquare_login"),
+    url(
+        '^login/callback/$',
+        foursquare_views.oauth2_callback,
+        name="foursquare_callback",
+    ),
+]
+
+
+twitter_urls = [
+    url('^login/$', twitter_views.oauth_login, name="twitter_login"),
+    url(
+        '^login/callback/$',
+        twitter_views.oauth_callback,
+        name="twitter_callback",
+    ),
+]
+
+
+social_urls = [
+    url('^login/cancelled/$', social_views.login_cancelled,
+        name='socialaccount_login_cancelled'),
+    url('^login/error/$', social_views.login_error, name='socialaccount_login_error'),
+    url('^signup/$', social_views.signup, name='socialaccount_signup'),
+    url('^connections/$', social_views.connections, name='socialaccount_connections')
+]
+
+
+allauth_urls = [
+    url(r"^signup/$", allauth_views.signup, name="account_signup"),
+    url(r"^login/$", allauth_views.login, name="account_login"),
+    url(r"^logout/$", allauth_views.logout, name="account_logout"),
+    url(r"^password/change/$", allauth_views.password_change,
+        name="account_change_password"),
+    url(r"^email/$", allauth_views.email, name="account_email"),
+    url(r"^confirm-email/$", allauth_views.email_verification_sent,
+        name="account_email_verification_sent"),
+    url(r"^confirm-email/(?P<key>[-:\w]+)/$", allauth_views.confirm_email,
+        name="account_confirm_email"),
+    url(r"^password/reset/$", allauth_views.password_reset,
+        name="account_reset_password"),
+    url(r"^password/reset/done/$", allauth_views.password_reset_done,
+        name="account_reset_password_done"),
+    url(r"^password/reset/key/(?P<uidb36>[0-9A-Za-z]+)-(?P<key>.+)/$",
+        allauth_views.password_reset_from_key,
+        name="account_reset_password_from_key"),
+    url(r"^password/reset/key/done/$", allauth_views.password_reset_from_key_done,
+        name="account_reset_password_from_key_done"),
+    url('^social/', include(social_urls)),
+    url('^twitter/', include(twitter_urls)),
+    url('^foursquare/', include(foursquare_urls)),
+]
+
+
 urlpatterns = [
     url(r'^api/', include(api_urls)),
     url(r'^docs/', include('rest_framework_swagger.urls')),
     url(r'^tags/', include(tag_urls)),
     url(r'^log/', include(log_urls)),
-    url(r'^allauth_login/', allauth_views.login, name='account_login'),
-    url(r'^accounts/', include('allauth.urls')),
+    url(r'^accounts/', include(allauth_urls)),
     url(r'^login/', allauth_views.login, name='login'),
-    url(r'^signup/', allauth_views.signup, name='account_signup'),
     url(
         r'^hold/',
         TemplateView.as_view(template_name='signup_success.html'),
