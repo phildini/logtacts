ba27b5e0ea4accb27aa176babd42eea86d5477e3
Philip James <pjj@philipjohnjames.com>
2016-10-09 12:08:10 -0700

    Improve test data script

--- contacts/management/commands/populate_test_data.py
+++ contacts/management/commands/populate_test_data.py
@@ -1,20 +1,108 @@
+import random
 from django.core.management.base import BaseCommand
+from django.utils import timezone
+from faker import Faker
+import contacts as contact_settings
+from contacts.models import (
+    Book,
+    BookOwner,
+    Contact,
+    ContactField,
+    Tag,
+    LogEntry,
+)
 
-from contacts.models import Book, Contact, ContactField
+
+fake = Faker()
+
 
 class Command(BaseCommand):
 
-    def handle(self, *args, **kwargs):
-        book = Book.objects.get(id=1)
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--book',
+            type=int,
+            dest='book',
+            default=1,
+            help='Book id to add generated contacts to',
+        )
+
+        parser.add_argument(
+            '--reset',
+            action='store_true',
+            dest='reset',
+            default=False,
+            help='Flag to reset the book at specified book id',
+        )
+
+    def handle(self, *args, **options):
+        book = Book.objects.get(id=options['book'])
+        user = BookOwner.objects.filter(book=book)[0].user
+        tags = []
+        if options['reset']:
+            Contact.objects.filter(book=book).delete()
+            Tag.objects.filter(book=book).delete()
+        for n in range(10):
+            tags.append(Tag.objects.create(
+                book=book,
+                tag=fake.word(),
+                color=fake.safe_hex_color(),
+            ))
         for n in range(40):
+            tag1 = random.choice(tags)
+            tag2 = random.choice(tags)
             contact = Contact.objects.create(
                 book=book,
-                name='Test {}'.format(n),
+                name=fake.name(),
             )
-            for m in range(10):
-                ContactField.objects.create(
+            contact.tags.add(tag1, tag2)
+            contact.save()
+
+            ContactField.objects.create(
+                contact=contact,
+                kind=contact_settings.FIELD_TYPE_ADDRESS,
+                label="Address",
+                value=fake.address(),
+            )
+            ContactField.objects.create(
+                contact=contact,
+                kind=contact_settings.FIELD_TYPE_TEXT,
+                label="Company",
+                value=fake.company(),
+            )
+            ContactField.objects.create(
+                contact=contact,
+                kind=contact_settings.FIELD_TYPE_EMAIL,
+                label="Email",
+                value=fake.email(),
+            )
+            ContactField.objects.create(
+                contact=contact,
+                kind=contact_settings.FIELD_TYPE_TEXT,
+                label="Job",
+                value=fake.job(),
+            )
+            ContactField.objects.create(
+                contact=contact,
+                kind=contact_settings.FIELD_TYPE_PHONE,
+                label="Phone",
+                value=fake.phone_number(),
+            )
+            ContactField.objects.create(
+                contact=contact,
+                kind=contact_settings.FIELD_TYPE_URL,
+                label="Website",
+                value=fake.url(),
+            )
+            for n in range(5):
+                log = LogEntry.objects.create(
                     contact=contact,
-                    kind='email',
-                    label='Test {}'.format(m),
-                    value='pjj+{}@pjj.pjj'.format(m),
+                    time=fake.date_time_this_year(
+                        tzinfo=timezone.get_current_timezone(),
+                    ),
+                    kind=random.choice(LogEntry.KIND_CHOICES),
+                    notes=fake.text(max_nb_chars=100),
+                    link=fake.url(),
+                    logged_by=user,
                 )
+                contact.update_last_contact_from_log(log)
